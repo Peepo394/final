b2a859a241d46ce38f60bb94f4b4731b50082446
Peepo394 <124545861+Peepo394@users.noreply.github.com>
2023-05-29 11:33:57 +0700

    Add files via upload

---
+++ bkktourpoint.js
@@ -0,0 +1,155 @@
+var bkkfoodpoint = {
+    "type": "FeatureCollection",
+    "features": [
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "เสน่ห์จันทน์",
+          "type": "อาหารไทยฟิวชั่น",
+          "level": "⭐⭐⭐⭐"
+        },
+        "geometry": {
+          "coordinates": [
+            100.54630730421306,
+            13.735642194827083
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "กอชุนฮวด",
+          "type": "อาหารจีน",
+          "level": "⭐⭐⭐⭐"
+        },
+        "geometry": {
+          "coordinates": [
+            100.48540013119406,
+            13.703765114675384
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "เธียนดอง",
+          "type": "อาหารเวียดนาม",
+          "level": "⭐⭐⭐⭐"
+        },
+        "geometry": {
+          "coordinates": [
+            100.53654682141337,
+            13.727150355335324
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "หน่องริมคลอง",
+          "type": "อาหารไทย",
+          "level": "⭐⭐⭐⭐"
+        },
+        "geometry": {
+          "coordinates": [
+            100.58868826610258,
+            13.741632960881764
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "ร้านแม่กลองหัวปลาหม้อไฟ",
+          "type": "อาหารไทย",
+          "level": "⭐⭐⭐⭐"
+        },
+        "geometry": {
+          "coordinates": [
+            100.42197029072264,
+            13.754674559687036
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "ร้านอาหารม่านเมือง",
+          "type": "อาหารเหนือ,อาหารไทย",
+          "level": "⭐⭐⭐⭐"
+        },
+        "geometry": {
+          "coordinates": [
+            100.67721994283112,
+            13.769136276548341
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "ขาหมูเจริญแสง สีลม",
+          "type": "อาหารไทย",
+          "level": "⭐⭐⭐⭐"
+        },
+        "geometry": {
+          "coordinates": [
+            100.51734383490403,
+            13.722832954411283
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "นายหมงหอยทอด",
+          "type": "อาหารไทย",
+          "level": "⭐⭐⭐⭐"
+        },
+        "geometry": {
+          "coordinates": [
+            100.51117046374085,
+            13.742388468906187
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "โจ๊กปรินซ์",
+          "type": "อาหารไทย-จีน",
+          "level": "⭐⭐⭐⭐"
+        },
+        "geometry": {
+          "coordinates": [
+            100.5165833330492,
+            13.721024917261884
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "คนชง คนปรุง",
+          "type": "อาหารไทย-จีน",
+          "level": "⭐⭐⭐⭐"
+        },
+        "geometry": {
+          "coordinates": [
+            100.55264885868397,
+            13.78916676139836
+          ],
+          "type": "Point"
+        }
+      }
+    ]
+  }
